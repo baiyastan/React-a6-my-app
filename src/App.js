40bc7077d4c01b7b775f8fbe4176ab6e1512b7f1
Bayastan <bsbaha195@gmail.com>
2023-11-21 10:28:38 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let title = "Hello";
+  title = "Hi";
+
+  function get() {
+    alert("Hello Okurmen");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={get} className="App">
+      {title} Aibek
     </div>
   );
 }
